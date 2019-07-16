3a782eab105e868663a24bb24b6251e1ddbde62f
Iury Vasconcelos <iury.vasconcelos@geq.com.br>
2019-07-16 11:34:44 -0300

    Adicionando o primeiro projeto perdido e continuando

---
+++ Alura Viagens/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  Alura Viagens
+//
+//  Created by Alura on 15/08/17.
+//  Copyright © 2017 Alura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tabelaViagens: UITableView!
+    @IBOutlet weak var viewHoteis: UIView!
+    @IBOutlet weak var viewPacotes: UIView!
+    
+    let listaViagens:Array<Viagem> = ViagemDAO().retornaTodasAsViagens()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tabelaViagens.dataSource = self
+        self.tabelaViagens.delegate = self
+        self.viewPacotes.layer.cornerRadius = 10
+        self.viewHoteis.layer.cornerRadius = 10
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listaViagens.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        let viagemAtual = listaViagens[indexPath.row]
+        
+        cell.labelTitulo.text = viagemAtual.titulo
+        cell.labelQuantidadeDias.text = "\(viagemAtual.quantidadeDeDias) dias"
+        cell.labelPreco.text = "R$ \(viagemAtual.preco)"
+        cell.imagemViagem.image = UIImage(named: viagemAtual.caminhoDaImagem)
+        
+        cell.imagemViagem.layer.cornerRadius = 10
+        cell.imagemViagem.layer.masksToBounds = true
+        
+        
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UIDevice.current.userInterfaceIdiom == UIUserInterfaceIdiom.phone ? 175 : 260
+    }
+
+}
+
