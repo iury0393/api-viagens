a11129865dcbcfdc147baa4b02e832c721a5fb6f
Iury Vasconcelos <iury.vasconcelos@geq.com.br>
2019-07-16 11:42:34 -0300

    Finalizando o projeto

--- Alura Viagens/ViewController.swift
+++ Alura Viagens/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    //MARK: - Outlets
+    
     @IBOutlet weak var tabelaViagens: UITableView!
     @IBOutlet weak var viewHoteis: UIView!
     @IBOutlet weak var viewPacotes: UIView!
     
+    //MARK: - Atributos
+    
     let listaViagens:Array<Viagem> = ViagemDAO().retornaTodasAsViagens()
+    
+    //MARK: - View Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,8 +32,9 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+    
+    //MARK: - TableView
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return listaViagens.count
@@ -36,15 +43,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
         let viagemAtual = listaViagens[indexPath.row]
-        
-        cell.labelTitulo.text = viagemAtual.titulo
-        cell.labelQuantidadeDias.text = "\(viagemAtual.quantidadeDeDias) dias"
-        cell.labelPreco.text = "R$ \(viagemAtual.preco)"
-        cell.imagemViagem.image = UIImage(named: viagemAtual.caminhoDaImagem)
-        
-        cell.imagemViagem.layer.cornerRadius = 10
-        cell.imagemViagem.layer.masksToBounds = true
-        
+        cell.configuraCelula(viagem: viagemAtual)
         
         return cell
     }
@@ -52,6 +51,5 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return UIDevice.current.userInterfaceIdiom == UIUserInterfaceIdiom.phone ? 175 : 260
     }
-
 }
 
